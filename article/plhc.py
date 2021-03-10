9bfae6f0b3ebda7cc8161860af0c2625ba838d90
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2021-03-10 14:13:43 -0500

    Update PLHC file with license and correct cases

--- article/plhc.py
+++ article/plhc.py
@@ -1,39 +1,64 @@
+# MIT License
+# 
+# Copyright (c) 2021 Pierre-Yves Camille Regis Taunay
+#  
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import numpy as np
-import h5py
-import pandas as pd
-
-import matplotlib.pyplot as plt
-import scipy.optimize as root
-
-from cathode.models.flow import poiseuille_flow, reynolds_number
-from cathode.models.taunay_et_al import solve
 import cathode.constants as cc
-
+from cathode.models.taunay_et_al import solve
 
 cat = 'PLHC'
+fname = 'plhc.h5'
 
-do_db = 0.22 * 25.4
-dc_db = 27.15
-M_db = 39.948
-Lo_db = 1.5
-eiz_db = 15.7596
+### Geometry in mm
+do_db = 0.22 * 25.4     # orifice diameter
+dc_db = 27.15           # insert diameter
+Lo_db = 1.5             # orifice length
 
-# Transform database results
+### Gas quantities: atomic mass and ionization potential
+species = 'Ar'
+M_db = cc.M.Ar
+eiz_db = 15.759
+
+### Transform to SI
 M = M_db * cc.atomic_mass
 dc = dc_db * 1e-3
 do = do_db * 1e-3
 Lo = Lo_db * 1e-3
-species = 'Ar'
 
+### Length of emitter and upstream pressure measurement
 Lupstream = (8+3/4) * 2.54e-2
-Lemitter = 8e-2
+Lemitter = 8.04e-2
 
-phisvec = np.array([1],dtype=np.float64)
+#############################
+### Run the actual cases ###
+############################
+phisvec = np.array([1,4,7,10],dtype=np.float64)         # Sheath voltages in V
+
 mdotvec = np.array([108.75],dtype=np.float64)
-Idvec = np.array([100],dtype=np.float64)
-
 mdotvec *= cc.sccm2eqA
-TgK = 3000.
 
-path, df = solve(Idvec, mdotvec, M_db, dc_db, do_db, Lo_db, Lupstream, Lemitter, eiz_db, TgK,
-                 'plhc.h5', verbose=True,phi_s=phisvec)
+Idvec = np.array([100,125,150,175,200,225,250,275,300,307],dtype=np.float64)
+
+dflist = []
+for TgK in [2000,3000,4000]:
+    path, df = solve(Idvec, mdotvec, M_db, dc_db, do_db, Lo_db, Lupstream, Lemitter, eiz_db, TgK,
+                     fname, verbose=True,phi_s=phisvec)
+    dflist.append(df)
